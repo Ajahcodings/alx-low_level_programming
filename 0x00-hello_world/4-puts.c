8539e7a48728e8b73ff3723716ed819806459b07
Ajahcodings <ajahchristchuks@gmail.com>
2023-02-19 05:26:44 -0800

    four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
+#include <stdio.h>
+
 /**
- * main - hello put
+ * main - entry c programming
  *
- * Description: using the main function
- * programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
 {
-	char ch;
-
-	for(ch = 'a' ; ch <= 'z' ; ch++)
+	char ch = 'a';
+	while (ch <= 'z')
 	{
-		if(ch != 'e' && ch != 'q')
+		if ((ch != 'e') && (ch != 'q'))
 		{
 			putchar(ch);
 		}
+		ch++;
 	}
 	putchar('\n');
 	return (0);
