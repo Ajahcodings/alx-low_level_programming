4badb1620d4c32c7525ae7722e5b39fc89ae5b2c
Ajahcodings <ajahchristchuks@gmail.com>
2023-02-21 17:48:42 -0800

    eight

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,15 +10,19 @@ void jack_bauer(void)
 {
 	int hour = 0, min;
 
-	while (min < 60)
+	while (hour < 24)
 	{
-		_putchar(hour / 10) + '0');
-		_putchar(hour % 10) + '0');
-		_putchar(':');
-		_putchar((min / 10) + '0');
-		_putchar((min % 10) + '0');
-		_putchar('\n');
-		min++;
+		min = 0;
+		while (min < 60)
+		{
+			_putchar(hour / 10) + '0');
+			_putchar(hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+			min++;
+		}
+		hour++;
 	}
-	hour++;
 }
