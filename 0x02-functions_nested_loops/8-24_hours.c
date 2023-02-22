2ce3b8d4457745ab68ce60d57b85827945c94d70
Ajahcodings <ajahchristchuks@gmail.com>
2023-02-21 17:41:28 -0800

    eight

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,3 +6,19 @@
  * Description: Using neste while loops,
  * count up through every minutes of the day and print 
  */
+void jack_bauer(void)
+{
+	int hour = 0, min;
+
+	while (min < 60)
+	{
+		_putchar(hour / 10) + '0');
+		_putchar(hour % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar('\n');
+		min++;
+	}
+	hour++;
+}
