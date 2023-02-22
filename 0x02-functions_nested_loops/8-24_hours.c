b64e830089d9a272f7ddd924c137cd1a2010882e
Ajahcodings <ajahchristchuks@gmail.com>
2023-02-21 17:54:38 -0800

    eight

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,7 @@
  * jack_bauer - a function that prints every minutes of the day of Jack B
  * starting from 00:00 to 23:59
  * Description: Using neste while loops,
- * count up through every minutes of the day and print 
+ * count up through every minute of the day and print 
  */
 void jack_bauer(void)
 {
