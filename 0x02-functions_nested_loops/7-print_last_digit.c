38e9f3e3adc81b7dae1973d31ca6bed83428c191
Ajahcodings <ajahchristchuks@gmail.com>
2023-02-21 17:18:45 -0800

    seven

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,6 +3,8 @@
 /**
  * print _last_digit - a function that prints the last digit of a number
  * @n: integer to be accepted
+ * Description: Determine last digit of n by
+ * isolating remainder of n / 10
  * Return: the value of the last digit
  */
 int print_last_digit(int n)
